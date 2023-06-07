b93cd052db5a8816ceddba23ce0e0fb677eb28e6
piggizze <ekoterko@gmail.com>
2023-06-07 15:22:38 +0200

    ajout pop up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,12 +8,23 @@ var closeBtn = document.getElementById("closeBtn");
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
-/* Set the width of the side navigation to 250px */
 function openNav() {
   sidenav.classList.add("active");
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
   sidenav.classList.remove("active");
 }
+
+//pop up//
+const openButton = document.getElementById('openButton');
+const popup = document.getElementById('popup');
+const closeButton = document.getElementById('closeButton');
+
+openButton.addEventListener('click', () => {
+  popup.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+  popup.style.display = 'none';
+});
